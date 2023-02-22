c0cc4a59afce72f6f588df944a949ad384bba626
gomljo <gomljo0307@naver.com>
2023-02-22 22:28:41 +0900

    feat(Header, _app): create common header for web site

--- next-bnb/pages/_app.tsx
+++ next-bnb/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from "next/app";
 import { GlobalStyleComponent } from "styled-components";
+import Header from "../components/Header";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const app = ({Component, pageProps}: AppProps) => {
     return (
         <>
             <GlobalStyle />
+            <Header />
             <Component {...pageProps} />
         </>
 
